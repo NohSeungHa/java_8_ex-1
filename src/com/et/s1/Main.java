750438a4d61bb60e06ef9ac78f9e71395081db3d
adt556 <adt556@daum.net>
2018-02-20 10:39:32 +0900

    Initial commit

---
+++ src/com/et/s1/Main.java
@@ -0,0 +1,14 @@
+package com.et.s1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SchoolController sct = new SchoolController();
+		System.out.println("프로그램 start");
+		sct.start();
+		
+
+	}
+
+}
---
+++ src/com/et/s1/SchoolController.java
@@ -0,0 +1,41 @@
+package com.et.s1;
+
+import java.util.Scanner;
+
+public class SchoolController {
+
+	//메서드명은 start 
+	//리턴은 없음
+	//내용은 1.학생등록 2.성적입력 3.성적조회 4.전체조회 5.프로그램종료
+
+	public void start() {
+		Scanner sc = new Scanner(System.in);
+		boolean check = true;	//전체프로그램 순환변수
+		
+		while(check) {
+			System.out.println("1.학생등록 2.성적입력 3.성적조회 4.전체조회 5.프로그램종료");	//메뉴출력
+			int select = sc.nextInt();
+
+			switch (select) {
+			case 1:
+				System.out.println("학생등록");
+				break;
+			case 2:
+				System.out.println("성적입력");
+				break;
+			case 3:
+				System.out.println("성적조회");
+				break;
+			case 4:
+				System.out.println("전체조회");
+				break;
+			case 5:
+				System.out.println("프로그램을 종료합니다.");
+				check = !check;
+				break;
+			default :
+				break;
+			}
+		}
+	}
+}
